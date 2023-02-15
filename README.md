aad5d291ca4cc1912799b16c4512b901a16d872a
jeanchaneto <92741347+jeanchaneto@users.noreply.github.com>
2023-02-15 11:02:39 +0100

    Initial work

---
+++ README.md
@@ -0,0 +1,39 @@
+# react-modal-jcto
+
+A simple responsive and customizable modal for React projects
+
+![Project Image](./images/mobile.png)
+
+## 1. Author
+
+Jean Chane-to
+
+
+## 2. Installation
+
+npm install react-modal-jcto
+
+
+## 3. Usage
+
+Styling and text content is passed on the compnent via 5 props:
+
+modalBackgroundStyle: pass inline styling for modal background
+
+modalBodyStyle: pass inline styling for modal body
+
+modalButtonStyle: pass inline styling for modal close button
+
+closeModalFunction: point your modal close function
+
+modalTextStyle: pass inline styling for modal text
+
+modalText: pass you modal text as a string 
+
+## 3. Example
+
+![Project Image](./images/example.png)
+
+Hope this helps!
+
+Happy programming!
---
+++ react-modal-jcto.jsx
@@ -0,0 +1,26 @@
+import "./modal.css"
+
+const Modal = ({ modalBackgroundStyle, modalBodyStyle, modalButtonStyle, closeModalFunction, modalTextStyle, modalText }) => {
+    return (
+        <div className="modal"
+            style={modalBackgroundStyle}>
+            <div className="modal-body"
+                style={modalBodyStyle}
+            >
+                <button className="modal-button"
+                    style={modalButtonStyle}
+                    onClick={closeModalFunction}
+                >
+                    X
+                </button>
+                <p className="modal-text"
+                    style={modalTextStyle}
+                >
+                    {modalText}
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default Modal;
